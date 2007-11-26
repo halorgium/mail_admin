480c47fc7a790baf158b8c03ef3b0226cf421914
Tim Carey-Smith <tim@spork.in>
2007-11-26 23:03:41 +1300

    Add the domain views

---
+++ app/controllers/domains_controller.rb
@@ -0,0 +1,5 @@
+class DomainsController < ApplicationController
+  make_resourceful do
+    actions :all
+  end
+end
---
+++ test/functional/domains_controller_test.rb
@@ -0,0 +1,10 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class DomainsControllerTest < ActionController::TestCase
+  tests DomainsController
+
+  # Replace this with your real tests.
+  def test_truth
+    assert true
+  end
+end
